90f3875f0ba85428aa62ad06e56dcccdd2a01b1b
apple <apple@appledemacbook-air.local>
2016-06-13 20:59:55 +0800

    新建工程并更改视图背景颜色

---
+++ GithubTest/GithubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GithubTest
+//
+//  Created by apple on 16/6/13.
+//  Copyright © 2016年 best. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GithubTest/GithubTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GithubTest
+//
+//  Created by apple on 16/6/13.
+//  Copyright © 2016年 best. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor=[UIColor greenColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
